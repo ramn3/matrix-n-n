20071d1ed94efb3b6e7efcc0d2b4b23871564131
t.mamedov@vke.su <bobkin36rus@mail.ru>
2024-05-10 20:33:46 +0300

    second_matrix

--- Roma/src/matrix_nn.py
+++ Roma/src/matrix_nn.py
@@ -4,14 +4,15 @@ def main():
               [-3, 7, 5]]
     m = []
     opr = 0
+    q = 1
     for k in range(3):
         for i in range(1, len(matrix)):
             for j in range(len(matrix)):
                 if j != k:
                     m.append(matrix[i][j])
-        print((-1)**k)
-        opr += (-1)**k * matrix[0][k] * (m[0] * m[3] - m[1] * m[2])
+        opr += q * matrix[0][k] * (m[0] * m[3] - m[1] * m[2])
         m.clear()
+        q *= -1
     print(opr)
 
 
@@ -25,19 +26,18 @@ def main():
     m = []
     for i in range(len(a)):
         matrix.append(list(map(int, a[i].split(', '))))
-    k = opr = 0
-    while k != len(matrix):
-        for i in range(len(matrix)):
+    opr = 0
+    q = 1
+    for k in range(3):
+        for i in range(1, len(matrix)):
             for j in range(len(matrix)):
-                if i > 0 and j != k:
+                if j != k:
                     m.append(matrix[i][j])
-        if len(m) == 4:
-            # print(matrix[0][k])
-            # print((m[0]*m[3]-m[1]*m[2]))
-            opr = opr + abs(matrix[0][k]) * (m[0] * m[3] - m[1] * m[2])
-            m.clear()
-        k = k + 1
+        opr += q * matrix[0][k] * (m[0] * m[3] - m[1] * m[2])
+        m.clear()
+        q *= -1
     print(opr)
+
 
 
 if __name__ == '__main__':
