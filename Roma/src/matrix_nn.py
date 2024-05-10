19b15f331ea493b52166dddaed908553d9ad4c47
t.mamedov@vke.su <bobkin36rus@mail.ru>
2024-05-10 20:28:34 +0300

    second_matrix

--- Roma/src/matrix_nn.py
+++ Roma/src/matrix_nn.py
@@ -3,18 +3,15 @@ def main():
               [1, 4, 0],
               [-3, 7, 5]]
     m = []
-    k = opr = 0
-    while k != len(matrix):
-        for i in range(len(matrix)):
+    opr = 0
+    for k in range(3):
+        for i in range(1, len(matrix)):
             for j in range(len(matrix)):
-                if i > 0 and j != k:
+                if j != k:
                     m.append(matrix[i][j])
-        if len(m) == 4:
-            # print(matrix[0][k])
-            # print((m[0]*m[3]-m[1]*m[2]))
-            opr = opr + abs(matrix[0][k]) * (m[0] * m[3] - m[1] * m[2])
-            m.clear()
-        k = k + 1
+        print((-1)**k)
+        opr += (-1)**k * matrix[0][k] * (m[0] * m[3] - m[1] * m[2])
+        m.clear()
     print(opr)
 
 
@@ -23,7 +20,7 @@ if __name__ == '__main__':
 
 
 def main():
-    a = open('../24.txt').readlines()
+    a = open('24.txt').readlines()
     matrix = []
     m = []
     for i in range(len(a)):
