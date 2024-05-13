5bd6520611f8a29f034276851a5b9d4180e8c47d
t.mamedov@vke.su <bobkin36rus@mail.ru>
2024-05-13 19:54:15 +0300

    second_matrix

---
+++ src/matrix-biblio.py
@@ -0,0 +1,5 @@
+import linalg
+import numpy as np
+A = np.matrix('2, -5, 3; 1, 4, 0; -3, 7, 5')
+print(A)
+print(np.linalg.det(A))
--- src/matrix_nn.py
+++ src/matrix_nn.py
@@ -1,26 +1,4 @@
 def main():
-    matrix = [[2, -5, 3],
-              [1, 4, 0],
-              [-3, 7, 5]]
-    m = []
-    opr = 0
-    q = 1
-    for k in range(3):
-        for i in range(1, len(matrix)):
-            for j in range(len(matrix)):
-                if j != k:
-                    m.append(matrix[i][j])
-        opr += q * matrix[0][k] * (m[0] * m[3] - m[1] * m[2])
-        m.clear()
-        q *= -1
-    print(opr)
-
-
-if __name__ == '__main__':
-    main()
-
-
-def main():
     a = open('24.txt').readlines()
     matrix = []
     m = []
@@ -39,6 +17,6 @@ def main():
     print(opr)
 
 
-
 if __name__ == '__main__':
     main()
+
